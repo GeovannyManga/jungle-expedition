8e3a6bc0203557c8e3e2a93f4b033d747b4f75fa
GeovannyManga <geovannyx.casimirom@unac.edu.co>
2025-12-07 23:54:38 -0500

    robot txt bien prueba 5

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -32,5 +32,11 @@ export default function sitemap(): MetadataRoute.Sitemap {
       changeFrequency: "yearly",
       priority: 0.5,
     },
+    {
+      url: "https://tayronacamping.com/robots.txt",
+      lastModified: new Date(),
+      changeFrequency: "yearly",
+      priority: 0.5,
+    },
   ];
 }
